0bd63e8d7c768199d57d5d2e559dc936aec7db2d
askari-reza <reza.779@gmail.com>
2022-08-15 21:43:11 +0430

    init

---
+++ modules/blog/index.ts
@@ -0,0 +1,29 @@
+import * as path from 'path';
+import { Module } from '@nuxt/types'
+
+
+const blogModule: Module = function() {
+  // Use this, this.options, this.nuxt
+  
+    // Automatically register all components of the module
+    this.nuxt.hook('components:dirs', (dirs: Array<object>) => { // ToDo: use correct type for func param
+      // Add ./components dir to the list
+      dirs.push({
+        path: path.resolve(__dirname, 'components'),
+      })      
+    })
+  
+    // Add module routes to global router list
+    this.extendRoutes((routes: Array<object>) => { // ToDo: use correct type for func param
+      routes.push({
+        name: 'blog',
+        path: '/blog',
+        component: path.resolve(__dirname, 'pages/blog.vue'),
+      })      
+    })
+  
+    // Register Store using a Plugin
+    this.addPlugin(path.resolve(__dirname, 'store/registerStore.js'))
+}
+
+export default blogModule
